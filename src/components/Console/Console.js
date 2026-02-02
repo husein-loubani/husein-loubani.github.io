816ea62d3b54df8e8cb7b5dff015caddec6729ea
husein-loubani <husein-loubani@github.com>
2026-02-02 14:17:14 +0100

    Fix console ASCII string for production build

--- src/components/Console/Console.js
+++ src/components/Console/Console.js
@@ -112,33 +112,25 @@ drwxr-xr-x  7 hloubani  phd  224 Dec 1 10:24 certifications/`
 │ Scholar  : scholar.google.com/citations?user=4IiL4OEAAAAJ  │
 │ Phone    : +33 7 50 00 39 07                                │
 └───────────────────────────────────────────────────────────┘`
-   },
-   {
-      command: 'echo "Machine Learning is my passion 🧠" | figlet',
-      result: ` 
-      
-      
-      
-
-    },
+   },  
     {
   command: 'neofetch',
-  result: `            .-/+oossssoo+/-.              hussein@ai-loubani
-        \`:+ssssssssssssssssss+:\`           ------------------------
-      -+ssssssssssssssssssyyssss+-         OS: AI Researcher 3.0
-    .ossssssssssssssssss/   /ossssso.      Host: UTBM / CIAD Lab
-   /sssssssssssssssssso/     /sssssss/     Kernel: Brain 9000X
-  +sssssssssssssssssss/       /ssssssso    Uptime: 25 years
- :ssssssssssssssssssss\\       /sssssssss:  Packages: PyTorch, CGAL, C++, Python
- +ssssssssssssssssssssso\\     /sssssssss+  Shell: Vision Terminal 2.3
- +sssssssssssssssssssssssooosssssssssss+  Resolution: 3D Mesh / LoD 2.3
- /ssssssssssssssssssssssssssssssssssssso  DE: Robotics & Perception
- .osssssssssssssssssssssssssssssssssssso  WM: Autonomous Navigation
-  -+sssssssssssssssssssssssssssssssss+-   Terminal: SLAM Shell
-   \\\\ossssssssssssssssssssssssssssso/     CPU: Deep Neural Cortex
-    \`.+ossssssssssssssssssssssssso+.\'      Memory: 2PB Virtual Worlds
-      \`-/+oossssssssssssssssoo+/-\`         Skills: [██████████] 98%
-        `
+  result: ` hussein@ai-loubani \\
+            ------------------------
+            OS: AI Researcher 3.0
+            Host: UTBM / CIAD Lab
+            Kernel: Brain 9000X
+            Uptime: 25 years
+            Packages: PyTorch, CGAL, C++, Python
+            Shell: Vision Terminal 2.3
+            Resolution: 3D Mesh / LoD 2.3
+            DE: Robotics & Perception
+            WM: Autonomous Navigation
+            Terminal: SLAM Shell
+            CPU: Deep Neural Cortex
+            Memory: 2PB Virtual Worlds
+            Skills: [██████████] 98%
+         `
 }
 
   ];
